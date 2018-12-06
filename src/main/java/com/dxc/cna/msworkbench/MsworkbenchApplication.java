96f9c8cfb837515e3377c8baad115bb5ea0790ca
eboopathy <eboopathy@ubuntu>
2018-12-06 11:44:58 +0000

    Committed all the changes including additions

---
+++ src/main/java/com/dxc/cna/msworkbench/MsworkbenchApplication.java
@@ -0,0 +1,12 @@
+package com.dxc.cna.msworkbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsworkbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsworkbenchApplication.class, args);
+	}
+}
