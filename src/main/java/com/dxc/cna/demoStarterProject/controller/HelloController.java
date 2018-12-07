2954e4513e9fa129901d9e8480ec40965a839a58
eboopathy <eboopathy@ubuntu>
2018-12-07 06:00:27 +0000

    Committed all the changes including additions

---
+++ src/main/java/com/dxc/cna/demoStarterProject/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.dxc.cna.demoStarterProject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/greet")
+    public String index() {
+        return "Congratulations!! DXC Cloud Native Starter Project is generated successfully!!Click on https://github.com/rituparnacse/Git.git to download your source code and enhance it. "; //"Greetings from Spring Starter Project !";
+    }
+
+}
